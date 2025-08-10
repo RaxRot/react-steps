f784490967031a28d67b6f00281b4c5dd1fd4573
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-10 21:38:46 +0100

    init ui

---
+++ ui/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+
+function App() {
+
+  return (
+    <>
+     <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
